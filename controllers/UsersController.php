e19c788b11f49713c1ffdd0262d9ecc73983211a
ChavdarPamukov <rabotenml@abv.bg>
2016-08-17 16:32:12 +0300

    Add login, register, logaut and home and post page

--- controllers/UsersController.php
+++ controllers/UsersController.php
@@ -4,16 +4,58 @@ class UsersController extends BaseController
 {
     public function register()
     {
-		// TODO: your user registration functionality will come here ...
+        if ($this->isPost){
+            $username = $_POST['username'];
+            if (strlen($username) < 2 || strlen($username) > 50){
+                $this->setValidationError("username", "Invalid username");
+            }
+            $password = $_POST['password'];
+            if (strlen($password) < 5 || strlen($password) > 50){
+                $this->setValidationError("password", "Invalid password");
+            }
+            $passwordRepeat = $_POST['password_repeat'];
+            if ($password != $passwordRepeat){
+                $this->setValidationError("password_repeat", "The two passwords did not match!");
+            }
+            $full_name = $_POST['full_name'];
+            if (strlen($full_name) > 200){
+                $this->setValidationError("password", "Invalid full name");
+            }
+            if ($this->formValid()){
+                $userId = $this->model->register($username, $password, $full_name);
+                if ($userId){
+                    $_SESSION['username'] = $username;
+                    $_SESSION['userId'] = $userId;
+                    $this->addInfoMessage("Registration successful.");
+                    $this->redirect("posts");
+                } else{
+                    $this->addErrorMessage("Error: user registration faild!");
+                }
+            }
+        }
     }
 
     public function login()
     {
-		// TODO: your user login functionality will come here ...
+        if ($this->isPost){
+            $username = $_POST['username'];
+            $password = $_POST['password'];
+            $loggedUserId = $this->model->login($username, $password);
+            if ($loggedUserId){
+                $_SESSION['username'] = $username;
+                $_SESSION['userId'] = $loggedUserId;
+                $this->addInfoMessage("Login successful.");
+                $this->redirect("posts");
+            }else{
+                $this->addErrorMessage("Error: login faild!");
+            }
+        }
     }
 
     public function logout()
     {
-		// TODO: your user logout functionality will come here ...
+        session_destroy();
+        $this->addInfoMessage("Login successful!");
+        $this->redirect("");
     }
 }
--- views/users/register.php
+++ views/users/register.php
@@ -2,4 +2,17 @@
 
 <h1><?= htmlspecialchars($this->title) ?></h1>
 
-<!-- TODO: register form will come here ... -->
+<form method="post">
+    <div><label for="username">Username:</label></div>
+    <input id="username" type="text" name="username" />
+    <div><label for="password">Password:</label></div>
+    <input id="password" type="password" name="password" />
+    <div><label for="password_repeat">Repeat password:</label></div>
+    <input id="password_repeat" type="password" name="password_repeat" />
+    <div><label for="username">Full name:</label></div>
+    <input id="full_name" type="text" name="full_name" />
+    <div>
+        <input type="submit" value="Register" />
+        <a href="<?=APP_ROOT?>/users/login"><i>Go to Login</i></a>
+    </div>
+</form>
